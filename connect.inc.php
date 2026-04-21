96a1206270a3f6e8ad7fc755e88efa1ccc78f8bb
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2026-04-21 23:25:19 +0200

    Ajout d'un fichier
    - Fichier config -> connect.inc.php

---
+++ connect.inc.php
@@ -0,0 +1,10 @@
+<?php
+// ============================================================
+// connect.inc.php — Paramètres de connexion à MariaDB
+// ============================================================
+
+$host     = "mysql03.univ-lyon2.fr";
+$login    = "php_mthomas5";
+$password = "iVe7ZqHEXrhiQ9279JBIHxYuG";
+$dbname   = $login;
+?>
